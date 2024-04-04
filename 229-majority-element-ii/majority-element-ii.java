714f0bc9ad450fc5ef36ae0e08fa8119adbebdb3
Gauravzz12 <gaurav1234thakurgt@gmail.com>
2024-04-04 23:23:47 +0530

    Time: 2 ms (99.25%) | Memory: 48.2 MB (27.42%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,20 +1,43 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> mp=new HashMap<>();
         List<Integer>ans=new ArrayList<>();
-        int n=nums.length/3;
+        int n=nums.length;
+        int ele1=Integer.MIN_VALUE,ele2=Integer.MIN_VALUE,cnt1=0,cnt2=0;
         for(int i:nums){
-            if(mp.containsKey(i)){
-                mp.put(i,mp.get(i)+1);
+            if(cnt1==0 && i!=ele2){
+                ele1=i;
+                cnt1=1;
+
             }
+            else if(cnt2==0 && i!=ele1){
+                ele2=i;
+                cnt2=1;
+                
+            }
+            else if(i==ele1)
+            cnt1++;
+            else if(i==ele2)
+            cnt2++;
             else
-            mp.put(i,1);
-        }
-        for(int i:mp.keySet()){
-            if(mp.get(i)>n){
-                ans.add(i);
+            {
+                cnt1--;
+                cnt2--;
             }
         }
+
+        cnt1=0;
+        cnt2=0;
+        int min=n/3;
+        for(int i:nums){
+            if(i==ele1)
+            cnt1++;
+            if(i==ele2)
+            cnt2++;
+        }
+        if(cnt1>min)
+        ans.add(ele1);
+        if(cnt2>min)
+        ans.add(ele2);
         return ans;
     }
 }
