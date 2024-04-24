66b701e3666f3e15822dee77ffa9100c99c44588
Gauravzz12 <gaurav1234thakurgt@gmail.com>
2024-04-24 10:26:13 +0530

    Time: 0 ms (100.00%) | Memory: 50 MB (83.96%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,34 +1,17 @@
 class Solution {
-    public int singleNonDuplicate(int[] arr) {
-       
-        int n=arr.length;
-                if (n == 1) return arr[0];
-    if (arr[0] != arr[1]) return arr[0];
-    if (arr[n - 1] != arr[n - 2]) return arr[n - 1];
-
-    int low = 1, high = n - 2;
-    while (low <= high) {
-        int mid = (low + high) / 2;
-
-        //if arr[mid] is the single element:
-        if (arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1]) {
-            return arr[mid];
+    public int singleNonDuplicate(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            if (nums[mid] != nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 2;
+            }
         }
-
-        //we are in the left:
-        if ((mid % 2 == 1 && arr[mid] == arr[mid - 1])
-                || (mid % 2 == 0 && arr[mid] == arr[mid + 1])) {
-            //eliminate the left half:
-            low = mid + 1;
-        }
-        //we are in the right:
-        else {
-            //eliminate the right half:
-            high = mid - 1;
-        }
+        return nums[left];
     }
-
-    // dummy return statement:
-    return -1;
-}
 }
