7b43d3d834bdc334625fe01c114b076534f0362a
Gaurav Thakur <gaurav1234thakurgt@gmail.com>
2024-09-07 23:40:48 +0530

    Time: 2 ms (89.72%) | Memory: 49.9 MB (16.85%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -4,12 +4,14 @@ class Solution {
         int max=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==1){
+                
             count++;
             if(count>max)
-            max=count;}
-            else
-            count=0;
-            
+                max=count;}
+            else{
+                
+                count=0;
+            }
         }
         return max;
     }
