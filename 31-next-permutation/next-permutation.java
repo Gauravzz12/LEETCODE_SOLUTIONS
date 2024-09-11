94396611a7e77fed7afa4e0c9128cdc63d390ccd
Gaurav Thakur <gaurav1234thakurgt@gmail.com>
2024-09-11 15:15:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (78.41%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,20 +1,15 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int breakpoint = -1;
-        
-        // Step 1: Find the breakpoint where nums[i] < nums[i+1]
-        for (int i = nums.length - 2; i >= 0; i--) {
+                for (int i = nums.length - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
                 breakpoint = i;
                 break;
             }
         }
-        
-        // Step 2: If no breakpoint found, reverse the entire array (i.e., last permutation case)
-        if (breakpoint == -1) {
+                if (breakpoint == -1) {
             reverse(nums, 0, nums.length - 1);
         } else {
-            // Step 3: Find the smallest element greater than nums[breakpoint] in the right subarray
             int nextgreater = -1;
             for (int i = nums.length - 1; i > breakpoint; i--) {
                 if (nums[i] > nums[breakpoint]) {
@@ -23,15 +18,11 @@ class Solution {
                 }
             }
             
-            // Step 4: Swap the elements at breakpoint and nextgreater
             swap(nums, breakpoint, nextgreater);
-            
-            // Step 5: Reverse the subarray after the breakpoint
             reverse(nums, breakpoint + 1, nums.length - 1);
         }
     }
 
-    // Helper method to reverse the array from index start to end
     private void reverse(int[] nums, int start, int end) {
         while (start < end) {
             swap(nums, start, end);
@@ -39,8 +30,6 @@ class Solution {
             end--;
         }
     }
-
-    // Helper method to swap two elements in the array
     private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
