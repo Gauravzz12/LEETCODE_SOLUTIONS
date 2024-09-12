602d35132a0c860c825c2a1fabd209f86ce52d35
Gaurav Thakur <gaurav1234thakurgt@gmail.com>
2024-09-12 11:47:01 +0530

    Time: 15 ms (14.32%) | Memory: 47.4 MB (81.24%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,43 +1,25 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer>ans=new ArrayList<>();
-        int n=nums.length;
-        int ele1=Integer.MIN_VALUE,ele2=Integer.MIN_VALUE,cnt1=0,cnt2=0;
-        for(int i:nums){
-            if(cnt1==0 && i!=ele2){
-                ele1=i;
-                cnt1=1;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        List<Integer> ans=  new ArrayList<>();
 
-            }
-            else if(cnt2==0 && i!=ele1){
-                ele2=i;
-                cnt2=1;
-                
-            }
-            else if(i==ele1)
-            cnt1++;
-            else if(i==ele2)
-            cnt2++;
-            else
-            {
-                cnt1--;
-                cnt2--;
-            }
-        }
-
-        cnt1=0;
-        cnt2=0;
-        int min=n/3;
         for(int i:nums){
-            if(i==ele1)
-            cnt1++;
-            if(i==ele2)
-            cnt2++;
+            if(!map.containsKey(i)){
+                map.put(i,1);
+            
+            }
+            else{
+                map.put(i,map.get(i)+1);
+            }
+
         }
-        if(cnt1>min)
-        ans.add(ele1);
-        if(cnt2>min)
-        ans.add(ele2);
+        int n=(int)(nums.length/3);
+        System.out.print(n);
+        map.forEach((k,v)->{
+            if(v>n){
+                ans.add(k);
+            }
+        });
         return ans;
     }
 }
