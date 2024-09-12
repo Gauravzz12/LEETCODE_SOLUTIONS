6688c44c5d11b3697049432612f41659a4154168
Gaurav Thakur <gaurav1234thakurgt@gmail.com>
2024-09-12 11:28:44 +0530

    Time: 1 ms (82.27%) | Memory: 42 MB (45.07%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,33 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
 
-        if (numRows == 0) {
+        if(numRows==0){
             return ans;
         }
-
-        // First row
-        List<Integer> firstRow = new ArrayList<>();
+        List<Integer> firstRow=new ArrayList<>();
         firstRow.add(1);
         ans.add(firstRow);
 
-        for (int i = 1; i < numRows; i++) {
-            List<Integer> prevRow = ans.get(i - 1);
-            List<Integer> currentRow = new ArrayList<>();
-
-            currentRow.add(1);
-
-            for (int j = 1; j < i; j++) {
-                int sum = prevRow.get(j - 1) + prevRow.get(j);
-                currentRow.add(sum);
+        for(int i=1;i<numRows;i++){
+            List<Integer> prevRow=ans.get(i-1);
+            List<Integer> curr=new ArrayList<>();
+            curr.add(1);
+            for(int j=1;j<i;j++){
+                int sum=prevRow.get(j-1)+prevRow.get(j);
+                curr.add(sum);
             }
-
-            currentRow.add(1);
-            ans.add(currentRow);
+            curr.add(1);
+            ans.add(curr);
         }
-
         return ans;
+
+
     }
 }
