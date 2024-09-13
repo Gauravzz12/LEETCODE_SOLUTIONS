6a17257c446b372e27abe22481c8f0f147ce1370
Gaurav Thakur <gaurav1234thakurgt@gmail.com>
2024-09-13 22:35:27 +0530

    Time: 14 ms (91.24%) | Memory: 43.9 MB (62.19%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,40 +1,34 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if (nums == null || nums.length < 4) return ans;  // Early return if input is invalid
-        
-        Arrays.sort(nums);  // Sort the array to simplify duplicate handling and two-pointer approach
-        
-        for (int i = 0; i < nums.length - 3; i++) {
-            // Skip duplicate 'i' elements
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            
-            for (int j = i + 1; j < nums.length - 2; j++) {
-                // Skip duplicate 'j' elements
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-                
-                int k = j + 1;
-                int l = nums.length - 1;
-                
-                while (k < l) {
-                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
-                    
-                    if (sum == target) {
-                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+        List<List<Integer>> ans=new ArrayList<>();
+         if (nums == null || nums.length < 4) return ans;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-3;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<nums.length-2;j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                int k=j+1;
+                int l=nums.length-1;
+                while(k<l){
+                 long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
+
+                    if(sum<target){
+                        k++;
+                    }
+                    else if(sum>target)l--;
+                    else{
+                        List<Integer> res=Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
+                        ans.add(res);
                         k++;
                         l--;
-                        
-                        // Skip duplicate 'k' and 'l' elements
-                        while (k < l && nums[k] == nums[k - 1]) k++;
-                        while (k < l && nums[l] == nums[l + 1]) l--;
-                    } else if (sum < target) {
-                        k++;
-                    } else {
-                        l--;
+
+                        while(k<l && nums[k]==nums[k-1])k++;
+                        while(k<l && nums[l]==nums[l+1])l--;
                     }
                 }
             }
         }
-        return ans;
+         return ans;
+
     }
 }
