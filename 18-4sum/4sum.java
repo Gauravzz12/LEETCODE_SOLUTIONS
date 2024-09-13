456285757e901bbaf91c07541ddea68d31e8d086
Gaurav Thakur <gaurav1234thakurgt@gmail.com>
2024-09-13 22:34:24 +0530

    Time: 18 ms (60.27%) | Memory: 43.7 MB (85.73%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,25 +1,32 @@
-import java.util.*;
-
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, long target) {
-        Arrays.sort(nums);
+    public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        int n = nums.length;
-        for (int i = 0; i < n - 3; i++) {
+        if (nums == null || nums.length < 4) return ans;  // Early return if input is invalid
+        
+        Arrays.sort(nums);  // Sort the array to simplify duplicate handling and two-pointer approach
+        
+        for (int i = 0; i < nums.length - 3; i++) {
+            // Skip duplicate 'i' elements
             if (i > 0 && nums[i] == nums[i - 1]) continue;
-            for (int j = i + 1; j < n - 2; j++) {
+            
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                // Skip duplicate 'j' elements
                 if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                
                 int k = j + 1;
-                int l = n - 1;
+                int l = nums.length - 1;
+                
                 while (k < l) {
                     long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                    
                     if (sum == target) {
-                        List<Integer> quad = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
-                        ans.add(quad);
-                        while (k < l && nums[k] == nums[k + 1]) k++;
-                        while (k < l && nums[l] == nums[l - 1]) l--;
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
                         k++;
                         l--;
+                        
+                        // Skip duplicate 'k' and 'l' elements
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
                     } else if (sum < target) {
                         k++;
                     } else {
