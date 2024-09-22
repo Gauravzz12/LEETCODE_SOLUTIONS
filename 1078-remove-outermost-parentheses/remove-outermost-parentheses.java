0256bea0c6a77ec4f9311b647372bb438ad9afaf
Gaurav Thakur <gaurav1234thakurgt@gmail.com>
2024-09-22 19:04:36 +0530

    Time: 14 ms (15.68%) | Memory: 45.5 MB (6.59%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -4,8 +4,6 @@ class Solution {
         int count=0;
        for(int i=0;i<s.length();i++){
         char c=s.charAt(i);
-            
-            
             if(c=='(' && count>=1){
             count++;
             ans+=c;}
